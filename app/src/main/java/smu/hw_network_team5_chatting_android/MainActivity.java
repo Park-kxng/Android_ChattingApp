70ec8352ec10d17d8ebbe6200f6324b1e64a91e6
Yujin Jeong <yujin45@sookmyung.ac.kr>
2022-12-01 15:05:07 +0900

    Initial commit

---
+++ app/src/main/java/smu/hw_network_team5_chatting_android/MainActivity.java
@@ -0,0 +1,33 @@
+package smu.hw_network_team5_chatting_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button connect;
+    private EditText ipAdress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ipAdress=(EditText) findViewById(R.id.editText1);
+        connect=(Button)findViewById(R.id.button1);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                String ip=ipAdress.getText().toString();
+                Client.SERVERIP=ip;
+                Intent intent = new Intent(getBaseContext(), ChattingActivity.class);
+                //Log.e("ServerIP", Client.SERVERIP);
+                startActivity(intent);
+            }
+        });
+    }
+}
